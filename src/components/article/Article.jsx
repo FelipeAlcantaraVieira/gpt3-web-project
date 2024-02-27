a6f3736ed0e4e3159ac5ca5e9958ece47007271f
Felipe AV <felipealcantaravieira@hotmail.com>
2024-02-27 14:28:13 -0300

    possibility created

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = (imgURL) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imgURL} alt="blog img" />
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './blog.css';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id="blog">
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgURL={blog01} />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imgURL={blog02} />
+          <Article imgURL={blog03} />
+          <Article imgURL={blog04} />
+          <Article imgURL={blog05} />
+        </div>
+      </div>
+    </div>
   )
 }
 
