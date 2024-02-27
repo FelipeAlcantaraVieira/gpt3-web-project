e1102978e1e9833b8499070e5e5c87690ffa2cb5
Felipe AV <felipealcantaravieira@hotmail.com>
2024-02-27 14:55:23 -0300

    blog section done

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './blog.css';
 import { Article } from '../../components';
 import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+import './blog.css';
 
 const Blog = () => {
   return (
@@ -11,13 +11,13 @@ const Blog = () => {
       </div>
       <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container_groupA'>
-          <Article imgURL={blog01} />
+          <Article imgURL={blog01} date="Sep 26, 2021" title ="GPT-3 and Open AI is the future. Let us explore how it is?" />
         </div>
         <div className='gpt3__blog-container_groupB'>
-          <Article imgURL={blog02} />
-          <Article imgURL={blog03} />
-          <Article imgURL={blog04} />
-          <Article imgURL={blog05} />
+          <Article imgURL={blog02} date="Sep 26, 2021" title ="GPT-3 and Open AI is the future. Let us explore how it is?" />
+          <Article imgURL={blog03} date="Sep 26, 2021" title ="GPT-3 and Open AI is the future. Let us explore how it is?" />
+          <Article imgURL={blog04} date="Sep 26, 2021" title ="GPT-3 and Open AI is the future. Let us explore how it is?" />
+          <Article imgURL={blog05} date="Sep 26, 2021" title ="GPT-3 and Open AI is the future. Let us explore how it is?" />
         </div>
       </div>
     </div>
